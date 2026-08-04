856449cd9dbc20af42b774e1a46d8bcf9892e35c
margueritedmbl-del <margueritedmbl@gmail.com>
2026-08-04 08:30:19 +0000

    Delete components directory

--- components/OfflineRegister.tsx
+++
@@ -1,6 +0,0 @@
-"use client";
-import { useEffect } from "react";
-export default function OfflineRegister(){
-  useEffect(()=>{ if("serviceWorker" in navigator) navigator.serviceWorker.register("/sw.js").catch(()=>{}); },[]);
-  return null;
-}
