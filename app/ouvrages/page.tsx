da5e432efde93b3d996c97160540956b04908e47
margueritedmbl-del <margueritedmbl@gmail.com>
2026-08-05 16:08:34 +0000

    Delete app directory

--- app/ouvrages/page.tsx
+++
@@ -1,8 +0,0 @@
-import DashboardShell from "@/components/DashboardShell";
-import OuvragesIntegratedDashboard from "@/components/ouvrages/OuvragesIntegratedDashboard";
-
-export default function OuvragesPage() {
-  return <DashboardShell title="Observatoire intégré des ouvrages" subtitle="Forages CRR et PM, piézomètres, analyses d’eau et micro-barrages réhabilités">
-    <OuvragesIntegratedDashboard />
-  </DashboardShell>;
-}
