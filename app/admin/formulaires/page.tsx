f61790e7b41ad4c3845632b57e26bb46125fed74
margueritedmbl-del <margueritedmbl@gmail.com>
2026-08-13 15:58:56 +0000

    Delete app directory

--- app/admin/formulaires/page.tsx
+++
@@ -1,3 +0,0 @@
-export const dynamic = "force-dynamic";
-import DashboardShell from "@/components/DashboardShell";import FormsManager from "@/components/admin/FormsManager";import RequireAuth from "@/components/auth/RequireAuth";import { ROLE_ADMIN, ROLE_SUPER_ADMIN } from "@/lib/permissions";
-export default function FormulairesPage(){return <RequireAuth allowedRoles={[ROLE_ADMIN, ROLE_SUPER_ADMIN]}><DashboardShell title="Formulaires Epicollect5" subtitle="Accès direct aux formulaires et sources API"><FormsManager/></DashboardShell></RequireAuth>}
