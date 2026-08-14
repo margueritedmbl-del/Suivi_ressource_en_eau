164e0cd3d4a48314ae84eb1315ef2033012204c5
margueritedmbl-del <margueritedmbl@gmail.com>
2026-08-14 09:35:46 +0000

    Add files via upload

---
+++ app/ouvrages/page.tsx
@@ -0,0 +1,8 @@
+import DashboardShell from "@/components/DashboardShell";
+import OuvragesIntegratedDashboard from "@/components/ouvrages/OuvragesIntegratedDashboard";
+
+export default function OuvragesPage() {
+  return <DashboardShell title="Observatoire intégré des ouvrages" subtitle="Forages CRR et PM, piézomètres, analyses d’eau et micro-barrages réhabilités — Version 4.4.1">
+    <OuvragesIntegratedDashboard />
+  </DashboardShell>;
+}
