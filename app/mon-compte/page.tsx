c6b37a1b8df9be6f32ee29095003a1504acc4096
margueritedmbl-del <margueritedmbl@gmail.com>
2026-09-09 17:32:06 +0000

    Delete app directory

--- app/mon-compte/page.tsx
+++
@@ -1,8 +0,0 @@
-export const dynamic = "force-dynamic";
-import DashboardShell from "@/components/DashboardShell";
-import RequireAuth from "@/components/auth/RequireAuth";
-import AccountPanel from "@/components/auth/AccountPanel";
-
-export default function MonComptePage() {
-  return <RequireAuth><DashboardShell title="Mon compte" subtitle="Sécurité, rôle et changement de mot de passe"><AccountPanel /></DashboardShell></RequireAuth>;
-}
